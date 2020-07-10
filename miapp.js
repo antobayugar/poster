55c9da29026c654e6f4f5726aeefa3dfd0a4c6c8
Gaston Dandre <gaston.dandre@gmail.com>
2020-07-10 16:24:12 -0300

    cambio de if y validaciones

--- miapp.js
+++ miapp.js
@@ -1,10 +1,10 @@
 //datos del nombre
 let nombre = prompt("Ingresá tu nombre:");
-let textoElement = document.getElementById('texto');
+let textoElement = document.getElementById("texto");
 //validacion simple, que no este vacio el prompt
 while (nombre == "") {
-    alert("El campo no puede estar vacío.");
-    nombre = prompt("Ingresá tu nombre:");
+  alert("El campo no puede estar vacío.");
+  nombre = prompt("Ingresá tu nombre:");
 }
 textoElement.innerText = nombre;
 
@@ -15,32 +15,42 @@ let tipografia = prompt("Elegí una tipografía: \n A. Serif \n B. San Serif");
 
 //validacion simple, que no este vacio el prompt
 function validacionTipografia(variable) {
-    while ((variable == "") || (variable != "A") || (variable != "a") || (variable != "B") || (variable != "b")) {
-        alert("La opción elegida no existe.");
-        variable = prompt("Elegí una tipografía: \n A. Serif \n B. San Serif");
-    }
-    return variable;
+  while (
+    variable == "" ||
+    variable != "A" ||
+    variable != "a" ||
+    variable != "B" ||
+    variable != "b"
+  ) {
+    alert("La opción elegida no existe.");
+    variable = prompt("Elegí una tipografía: \n A. Serif \n B. San Serif");
+  }
+  return variable;
 }
 
 //eleccion tipografia segun respuesta
-if (tipografia == "B" || tipografia == "b") {
-    textoElement.classList.add("sans");
+if (tipografia.toLowerCase() == "a") {
+  textoElement.classList.add("sans");
+} else if (tipografia.toLowerCase() == "b") {
+  textoElement.classList.add("serif");
 } else {
-    textoElement.classList.add("serif");
+  alert("No se eligió ninguna opción.");
 }
-
 //eleccion tematica: fondo imagen + color texto
-let tematica = prompt("Elegí una temática: \n A. Marítima \n B. Bosque \n C. Oscuro");
-let bodyElement = document.getElementById('body');
+let tematica = prompt(
+  "Elegí una temática: \n A. Marítima \n B. Bosque \n C. Oscuro"
+);
+let bodyElement = document.getElementById("body");
 
-if ((tematica == "A") || (tematica == "a")) {
-    bodyElement.classList.add('fondo-maritimo');
-    textoElement.classList.add('texto-maritimo');
-} else 
-if((tematica == "B") || (tematica == "b")) {
-    bodyElement.classList.add('fondo-bosque');
-    textoElement.classList.add('texto-bosque');
+if (tematica.toLowerCase() == "a") {
+  bodyElement.classList.add("fondo-maritimo");
+  textoElement.classList.add("texto-maritimo");
+} else if (tematica.toLowerCase() == "b") {
+  bodyElement.classList.add("fondo-bosque");
+  textoElement.classList.add("texto-bosque");
+} else if (tematica.toLowerCase() == "c") {
+  bodyElement.classList.add("fondo-oscuro");
+  textoElement.classList.add("texto-oscuro");
 } else {
-    bodyElement.classList.add('fondo-oscuro');
-    textoElement.classList.add('texto-oscuro');
-} 
+  alert("No se eligió ninguna opción");
+}
